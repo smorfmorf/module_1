ca22040bfba4ae88b5b29e23453e12d00732f6d3
mazaka <malalamobilelegends@gmail.com>
2023-07-01 01:04:31 +1000

    Второй товар

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const laptop = "asus";
-let count = 4;
-const category = "notebook";
-const price = +"600";
+{
+    const laptop = "asus";
+    let count = 4;
+    const category = "notebook";
+    const price = +"600";
 
-console.log(`Наименование товара: ${laptop}`);
-console.log(`Общая сумма товара ${price * count}`);
+    console.log(`Наименование товара: ${laptop}`);
+    console.log(`Общая сумма товара ${price * count}`);
+}
+
+{
+    const item = "smart проектор philips";
+    const count = 15;
+    const category = "видео техника";
+    const price = 1200;
+
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+}
