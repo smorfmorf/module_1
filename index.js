c7a45b347492dbad537c753e00da73f586657dd2
mazaka <malalamobilelegends@gmail.com>
2023-06-30 19:15:30 +1000

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const laptop = "asus";
+let count = 4;
+const category = "notebook";
+const price = +"600";
+
+console.log(`Наименование товара: ${laptop}`);
+console.log(`Общая сумма товара ${price * count}`);
